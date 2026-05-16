dcff7468271ea901bc29a06e62d21eebe6492a9c
LemurDaniel <Landau.Daniel.1998@gmail.com>
2026-05-16 20:09:07 +0200

    scaffolding

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+#include <MiniServer.h>
+
+#include <routes/routes.server.manager.h>
+
+void setup()
+{
+  Serial.begin(115200);
+
+  EspWeb::MiniServer *Server = new EspWeb::MiniServer();
+
+  // Will start enter WiFi setup, if this function isn't used.
+  // Credentials are permanently stored via LittleFs.
+  // Server->connectWiFi("<SSID / Wlan Name >", "***<PASSWORD>***");
+
+  // For testing purposes, remove WiFi config to trigger AP mode
+  // Server->clearWiFi();
+
+  // Hardcode default credentials (Can be set via Dashoard without hardcoding!)
+  // Server->defaultAdminCredentials("admin", "admin");
+  // Server->defaultAdminSalt("");
+
+  // Disables admin routes entirly
+  // Server->disableAdmin();
+
+  // Server->root("/web");
+  // Server->index("/web/index.html");
+
+  Server->registerRouter(routes_server_manager::Router());
+
+  Server->dns("dev"); // dev.local
+  // Server->dns("web"); // web.local
+
+  Server->start(80);
+}
+
+void loop()
+{
+  while (true)
+  {
+    delay(10);
+  }
+}
